b03e7263a3d6f341b6bec7a84215e32c0fbc6ae5
John Allen Dugelio <dugeliojohnallen@gmail.com>
2023-11-10 22:01:09 +0700

    delete files
    delete files

--- Config/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    define('ROOT_URL', 'http://localhost/recordsapp/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS','allen');
-    define('DB_NAME','recordsapp_db');
-?>
